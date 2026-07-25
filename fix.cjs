0ac949e5e0194204230e5f870a18afe9765fe669
SajidCodeX <sajidaliansari091@gmail.com>
2026-07-25 22:40:38 +0530

    chore: Update .gitignore for clean production deployment

--- fix.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-;['src/lib/auth.ts', 'src/lib/admin.ts', 'src/lib/dashboard.ts'].forEach(f => {
-  let code = fs.readFileSync(f, 'utf8')
-  code = code.replace(/\.validator\(/g, '.inputValidator(')
-  fs.writeFileSync(f, code)
-})
-
-console.log('Fixed inputValidator')
--- scratch/test.js
+++
@@ -1,3 +0,0 @@
-const { getTenantDb } = require('./src/lib/server-utils.server.ts');
-// actually we can't easily call createTeamInvite outside of TanStack Start context because requireAuth requires headers.
-// But we can check if it's the builderId or email unique constraint.
