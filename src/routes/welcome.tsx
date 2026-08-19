24cb03fd799bca2c03d091d685c3ef39f46a1411
SajidCodeX <sajidaliansari091@gmail.com>
2026-08-19 23:24:28 +0530

    fix: blend image background to exact website black #060608 and switch to transparent alpha PNG to eliminate bluish box artifacts

--- src/routes/welcome.tsx
+++ src/routes/welcome.tsx
@@ -417,7 +417,7 @@ function WelcomePage() {
               className="relative w-full flex items-center justify-center"
             >
               <img
-                src="/images/exploded-villa.jpg"
+                src="/images/exploded-villa.png"
                 alt="WeaverFrame Exploded Luxury Architecture"
                 className="w-full max-w-[1250px] xl:scale-115 xl:translate-x-4 h-auto object-contain select-none pointer-events-none drop-shadow-[0_30px_70px_rgba(0,0,0,0.95)] transition-transform duration-700 hover:scale-[1.18]"
               />
